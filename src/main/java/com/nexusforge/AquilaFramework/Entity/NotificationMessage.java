386ebe9d075cbbfd72a9669a9db9c37b8021aa9c
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-06-29 19:31:35 +0630

    add

---
+++ src/main/java/com/nexusforge/AquilaFramework/Entity/NotificationMessage.java
@@ -0,0 +1,12 @@
+package com.nexusforge.AquilaFramework.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NotificationMessage {
+
+    private String message;
+    private String timestamp;
+}
